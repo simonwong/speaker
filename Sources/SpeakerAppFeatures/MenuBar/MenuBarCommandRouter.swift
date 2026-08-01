2fa90c237fa0f70b7ec6698ef377be55ccbeafeb
simonwong <wsj_simon@163.com>
2026-08-01 19:22:34 +0800

    feat: simplify the menu bar shortcuts (#19)

--- Sources/SpeakerAppFeatures/MenuBar/MenuBarCommandRouter.swift
+++ Sources/SpeakerAppFeatures/MenuBar/MenuBarCommandRouter.swift
@@ -1,51 +1,51 @@
 @MainActor
 package enum MenuBarCommand: Equatable, Sendable {
+    case overview
     case permissionSettings
-    case history
     case settings
-    case about
+    case dataErasureRecovery
     case quit
 }
 
 @MainActor
 package struct MenuBarCommandRouter {
     private let navigation: SettingsNavigationModel
+    private let openOverview: () -> Void
     private let openSettings: () -> Void
-    private let openAbout: () -> Void
-    private let openHistory: () -> Void
+    private let openDataErasureRecovery: () -> Void
     private let activate: () -> Void
     private let terminate: () -> Void
 
     package init(
         navigation: SettingsNavigationModel,
+        openOverview: @escaping () -> Void,
         openSettings: @escaping () -> Void,
-        openAbout: @escaping () -> Void,
-        openHistory: @escaping () -> Void,
+        openDataErasureRecovery: @escaping () -> Void,
         activate: @escaping () -> Void,
         terminate: @escaping () -> Void
     ) {
         self.navigation = navigation
+        self.openOverview = openOverview
         self.openSettings = openSettings
-        self.openAbout = openAbout
-        self.openHistory = openHistory
+        self.openDataErasureRecovery = openDataErasureRecovery
         self.activate = activate
         self.terminate = terminate
     }
 
     package func perform(_ command: MenuBarCommand) {
         switch command {
+        case .overview:
+            openOverview()
+            activate()
         case .permissionSettings:
             navigation.open(.permissions)
             openSettings()
             activate()
-        case .history:
-            openHistory()
-            activate()
         case .settings:
             openSettings()
             activate()
-        case .about:
-            openAbout()
+        case .dataErasureRecovery:
+            openDataErasureRecovery()
             activate()
         case .quit:
             terminate()
